364db32de2fe3d4b63757c4a866725a7d635b876
aidenhu <99224270+CS2Student@users.noreply.github.com>
2024-01-13 18:32:33 -0500

    push to github

--- mongo.js
+++ mongo.js
@@ -1,38 +1,38 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = 
-    `mongodb+srv://fullstack:${password}@cluster0.g32frhw.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.g32frhw.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//     content: 'Mongoose makes things easy',
-//     important: true,
-// })
-
-// note.save().then (result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
-
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+const note = new Note({
+  content: 'Browser can execute only JavaScript',
+  important: true,
 })
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
